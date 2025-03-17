e7cb627fe89e337fdf03f72a737eb24aaf671b92
vishnureddy <tarigopula34@gmail.com>
2025-03-17 15:01:25 +0530

    catalogue script is executed

---
+++ 06-if-condition.sh
@@ -0,0 +1,37 @@
+# If found in 3 forms
+# 1. Simple if
+
+#if [ expression ]; then
+#  commands
+#fi
+
+# 2. If Else
+
+#if [ expression ]; then
+#  commands-T
+#else
+#  commands-F
+#fi
+
+# 3. Else if
+
+#if [ expression-a ]; then
+#  commands-a
+#elif [ expression-b ]; then
+#  commands-b
+#elif [ expression-c ]; then
+#  commands-c
+#else
+#  commands
+#fi
+
+# Expressions
+# Strings
+# [ abc == bac ]
+# [ $a == $x ]
+# Operators : == , !=, -z
+
+# Numbers
+# -eq , -ne , -ge , -le, -le
+# Files
+
